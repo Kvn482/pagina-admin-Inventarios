6a0cf68c93becc4455fb39762f851097e8be0d9e
Kvn482 <kevinacostaespinoza@gmail.com>
2025-05-03 23:28:41 -0700

    Add login component

--- FrontEnd/FrontEnd/src/app/app.component.ts
+++ FrontEnd/FrontEnd/src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'FrontEnd';
+
+  constructor(private router: Router) {}
+
+  goToLogin() {
+    this.router.navigate(['/login']);
+  }
 }
